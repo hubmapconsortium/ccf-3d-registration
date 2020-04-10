8c3791b81646595123da74d17f56032166aa1058
andreasbueckle <abueckle@indiana.edu>
2020-04-10 12:30:10 -0400

    Updates

---
+++ FunctionTesting.js
@@ -0,0 +1,10 @@
+window.hideSignupScreen = true;
+
+
+window.ruiRegistrationCallback = function (str) {
+    alert("I got json!" + "\n" + str);
+}
+
+/* <script src="FunctionTesting.js"></script> */
+
+// file:///C:/Users/abueckle/Desktop/RUI-Web/output/index.html
